4d5087c75dfbb59c9947d904e6b6fe1aa8e6efad
Christian Popescu <christian.popescu@outlook.com>
2021-11-13 18:24:00 +0100

    Simple terminal output only application

--- Java/Lanterna/src/SanboxTerminal/src/main/java/fr/vadc/java/sandbox/App.java
+++ Java/Lanterna/src/SanboxTerminal/src/main/java/fr/vadc/java/sandbox/App.java
@@ -1,13 +1,40 @@
 package fr.vadc.java.sandbox;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+import java.text.MessageFormat;
 
 /**
- * Hello world!
+ * Terminal !
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args )    {
+
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            SomeTerminalStaticText(terminal);
+        } catch (java.io.IOException e) {
+            System.out.println("Can not initialize Default Terminal");
+        }
+    }
+
+    private static void SomeTerminalStaticText(Terminal terminal) throws IOException {
+        terminal.setCursorPosition(10, 5);
+        terminal.setForegroundColor(TextColor.ANSI.RED);
+        terminal.setBackgroundColor(TextColor.ANSI.BLUE);
+        terminal.putCharacter('H');
+        terminal.putCharacter('e');
+        terminal.putCharacter('l');
+        terminal.putCharacter('l');
+        terminal.putCharacter('o');
+        terminal.putCharacter('!');
+        terminal.setCursorPosition(0, 0);
+        TerminalSize screenSize = terminal.getTerminalSize();
+        terminal.putString(MessageFormat.format("Screen size : rows = {0} columns = {1}", screenSize.getRows(), screenSize.getColumns()));
     }
 }
