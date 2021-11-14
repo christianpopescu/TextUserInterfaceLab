98234e5fa956e7d8c9f5f7a7af199a5babed5c86
Christian Popescu <christian.popescu@outlook.com>
2021-11-14 17:00:18 +0100

    Jansi first version hello world with collors

--- Java/Jansi/SandboxJansi/src/main/java/fr/vadc/java/sandbox/SandboxJansiApp.java
+++ Java/Jansi/SandboxJansi/src/main/java/fr/vadc/java/sandbox/SandboxJansiApp.java
@@ -1,4 +1,7 @@
 package fr.vadc.java.sandbox;
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
 
 /**
  * Hello world!
@@ -8,6 +11,10 @@ public class SandboxJansiApp
 {
     public static void main( String[] args )
     {
+        AnsiConsole.systemInstall();
+
+        System.out.println( ansi().eraseScreen().fg(RED).a("Hello").fg(GREEN).a(" World").reset());
         System.out.println( "Hello World!" );
+        AnsiConsole.systemUninstall();
     }
 }
