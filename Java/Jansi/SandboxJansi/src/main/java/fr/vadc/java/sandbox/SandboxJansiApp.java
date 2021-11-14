c4a7908ac5a6636c1dc083744d692b51a794d4d8
Christian Popescu <christian.popescu@outlook.com>
2021-11-14 15:28:10 +0100

    Add Jansi Sandbox application

---
+++ Java/Jansi/SandboxJansi/src/main/java/fr/vadc/java/sandbox/SandboxJansiApp.java
@@ -0,0 +1,13 @@
+package fr.vadc.java.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class SandboxJansiApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Java/Jansi/SandboxJansi/src/test/java/fr/vadc/java/sandbox/SandboxJansiAppTest.java
@@ -0,0 +1,20 @@
+package fr.vadc.java.sandbox;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SandboxJansiAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
